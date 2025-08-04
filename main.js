7e28f2f2534be7a9b645cd70c68bd0d6cc373121
𝐑𝐎𝐘𝐀𝐋 𝐃𝐑𝐄𝐀𝐌 | VRP <b0f4382348f1e0e@gmail.com>
2025-08-04 07:41:55 +0300

    Update and rename military.html to main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('renderer/index.html');
+}
+
+app.whenReady().then(createWindow);
