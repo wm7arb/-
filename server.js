a88305a0d0bb4c2085ad1eba38f3048cf2ea93ee
𝐑𝐎𝐘𝐀𝐋 𝐃𝐑𝐄𝐀𝐌 | VRP <b0f4382348f1e0e@gmail.com>
2025-09-30 21:44:51 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-app.use(express.static(__dirname));
-
-app.listen(PORT, () => {
-  console.log(`🚀 CR Token Manager running at http://localhost:${PORT}`);
-});
